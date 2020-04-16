44c2eb2a41275c168aef8d4dc257a876bcec645e
Gabriel Paolo Tiu <paolotiu17@gmail.com>
2020-04-16 16:09:34 +0800

    added reset button

--- script.js
+++ script.js
@@ -8,11 +8,29 @@ function makeGrid(rows, columns){
     for (i = 0; i < (rows * columns); i++){
         let cell = document.createElement('div')
         cell.innerText = i + 1;
-        cell.addEventListener('mouseover', () => cell.style.backgroundColor = "lightblue")
+        cell.addEventListener('mouseover', () => cell.style.backgroundColor = `rgb(${randomColor()},${randomColor()},${randomColor()})`)
         gridContainer.appendChild(cell).className = "grid-item"
-        
-
     }
 }
+
+function randomColor (){
+    return Math.floor(Math.random() * 255)
+}
+
 makeGrid(16, 16)
+
+const reset = document.getElementById('reset')
+
+reset.onclick = function (){
+
+    let grid = prompt('How many sqaures per side?');
+    grid = parseInt(grid);
+        
+    if((grid == null) || (typeof(grid) != "number") || (isNaN(grid))){
+        return null
+    }
+
+    gridContainer.innerHTML = '';
+    makeGrid(grid, grid)
+}
 
